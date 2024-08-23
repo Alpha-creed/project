be3a211a30da2ced4fedf8d3101e70f2385ebd9d
Alpha-creed <wynoral@gmail.com>
2024-08-23 00:17:27 +0000

    initial commit--progress

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    { name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently building a waste ordering app",
+      description: "The Inside Scoop",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "3D Solar System Planets to Explore",
+      des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+      img: "/p1.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+      link: "https://github.com/adrianhajdin?tab=repositories",
+    },
+    {
+      id: 2,
+      title: "Yoom - Video Conferencing App",
+      des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+      img: "/p2.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/zoom-clone",
+    },
+    {
+      id: 3,
+      title: "AI Image SaaS - Canva Application",
+      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
+      img: "/p3.svg",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/adrianhajdin/ai_saas_app",
+    },
+    {
+      id: 4,
+      title: "Animated Apple Iphone 3D Website",
+      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+      img: "/p4.svg",
+      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+      link: "https://github.com/adrianhajdin/iphone",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Highly recommend enchill! He delivered a beautifully designed and responsive site on time, and his ability to understand and implement our vision was outstanding.",
+      name: "Thelma Mensah",
+      title: "CEO of Cologne Craft Perfumes",
+    },
+    {
+      quote:
+        "Professional and efficient! The developer not only met our design requirements but also provided valuable suggestions that enhanced the overall functionality of our site.",
+      name: "Veronica enchill",
+      title: "Director of Vees Shop",
+    },
+    {
+      quote:
+        "Impressive work! Mr. Enchill's expertise in website development and his dedication to delivering a high-quality product made him a pleasure to work with. Our new website is both visually outstanding and functions flawlessly.",
+      name: "Eugenia Owusu",
+      title: "HR for Owusu Cakes & Coffee",
+    },
+    {
+      quote:
+        "Collaborating with enchill was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. enchill's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, enchill is the ideal partner.",
+      name: "Joe Cassidy",
+      title: "Backend Software Developer",
+    },
+    {
+      quote:
+       " Without doubt one of the most talented programmers out there.  I always go back to Warren when I'm out of my depth and he's never failed to deliver what I ask for.   Smart, trustworthy and professional.   You won't be disappointed.",
+      name: "Susan Brown",
+      title: "Director of Brown's loans and microfinance",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "Cologne Craft",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "Vees Shop",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "Codify Central",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "Browns Microfinance",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "Owusu Cakes and Coffee.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "IT Technician - Felix Computers",
+      desc: "Provided computer repair and sales services in Sekondi, Ghana, during the COVID-19 pandemic.",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Frontend Engineer Intern",
+      desc: "Acquired expertise in React and developed a restaurant website in collaboration with programmers from various African countries during the Side Hustle internship",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: " Frontend Developer",
+      desc: "Collaborated with a startup to develop solutions tailored for businesses and companies.",
+      className: "md:col-span-2", // change to md:col-span-2
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Freelance Software Developer",
+      desc: "Work as a Freelance Software Developer, creating custom software solutions and consulting services tailored to clients' business needs.",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+    },
+    {
+      id: 2,
+      img: "/twit.svg",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+    },
+  ];
